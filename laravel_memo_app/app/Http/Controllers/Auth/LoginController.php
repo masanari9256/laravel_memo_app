4b57dbb2f3bcaafcb051f23d76f31d07d809ab5d
masanari9256 <masanari92562@gmail.com>
2022-02-06 17:22:44 +0900

    [update]ログイン機能をカスタマイズ

--- laravel_memo_app/app/Http/Controllers/Auth/LoginController.php
+++ laravel_memo_app/app/Http/Controllers/Auth/LoginController.php
@@ -5,36 +5,52 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
+  /*
+  |--------------------------------------------------------------------------
+  | Login Controller
+  |--------------------------------------------------------------------------
+  |
+  | This controller handles authenticating users for the application and
+  | redirecting them to your home screen. The controller uses a trait
+  | to conveniently provide its functionality to your applications.
+  |
+  */
 
-    use AuthenticatesUsers;
+  use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
+  /**
+   * Where to redirect users after login.
+   *
+   * @var string
+   */
+  protected $redirectTo = '/memo';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
-    }
+  /**
+   * ログイン時のバリデーション
+   * @param Request $request
+   */
+  public function validateLogin( Request $request) {
+    $request->validate(
+      [
+        $this->username() => 'required | max:255 | email',
+        'password' => 'required|min:8|max:255|regex:/^[a-zA-Z0-9]+$/',
+      ],
+      [
+        'password.regex' => ':attributeは半角英数字で入力してください。'
+      ]
+    );
+  }
+
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct() {
+    $this->middleware('guest')->except('logout');
+  }
 }
