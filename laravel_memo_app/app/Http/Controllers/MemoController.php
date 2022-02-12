7a10050aa433111812c4548bb27f2f1004ca581d
masanari9256 <masanari92562@gmail.com>
2022-02-12 15:14:15 +0900

    [update]メモ追加機能を実装

--- laravel_memo_app/app/Http/Controllers/MemoController.php
+++ laravel_memo_app/app/Http/Controllers/MemoController.php
@@ -12,7 +12,13 @@ class MemoController extends Controller
    * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
    */
   public function index() {
-    return view('memo');
+    $memos = Memo::where('user_id', \Auth::id())->orderBy('updated_at', 'desc')->get();
+    // $memos = \DB::table('memos')->where('user_id', \Auth::id())->orderBy('updated_ate', 'desc')->get();
+
+    return view('memo', [
+      'name' => $this->getLoginUserName(),
+      'memos' => $memos
+    ]);
   }
 
   public function add() {
@@ -23,5 +29,24 @@ class MemoController extends Controller
     ]);
 
     return redirect()->route('memo.index');
+  }
+
+  /**
+   * ログインユーザー名の取得
+   * @return string
+   */
+  private function getLoginUserName() {
+    $user = \Auth::user();
+
+    $name = '';
+    if($user) {
+      if(7 < mb_strlen($user->name)) {
+        $name = mb_substr($user->name, 0, 7) . "...";
+      } else {
+        $name = $user->name;
+      }
+    }
+
+    return $name;
   }
 }
